c280a669e8bfa953f5dc4ebd3a7431c7db9355b0
rowhe <dpopov.pro@yandex.ru>
2023-09-28 21:14:32 +0300

    practice 5 part 2

--- Графы и деревья/Лабораторная 5/Платная лестница. Дейкстра/task.py
+++ Графы и деревья/Лабораторная 5/Платная лестница. Дейкстра/task.py
@@ -11,10 +11,25 @@ def stairway_path(graph: nx.DiGraph) -> Union[float, int]:
     :param graph: Взвешенный направленный граф NetworkX, по которому надо рассчитать стоимости кратчайших путей
     :return: минимальная стоимость подъема на верхнюю ступень
     """
-    ...  # TODO c помощью функции из модуля networkx найти стоимость кратчайшего пути до последней лестницы
+    # c помощью функции из модуля networkx найти стоимость кратчайшего пути до последней лестницы
+    min = nx.dijkstra_path_length(graph, 0, graph.number_of_nodes()-1)
+    return min
 
 
 if __name__ == '__main__':
     stairway = (5, 11, 43, 2, 23, 43, 22, 12, 6, 8)
-    stairway_graph = ...  # TODO записать взвешенный граф, а лучше написать функцию, которая формирует граф по стоимости ступеней
+    # записать взвешенный граф, а лучше написать функцию, которая формирует граф по стоимости ступеней
+
+    stairway_graph = nx.DiGraph()
+
+    _list = []
+
+    for i in range(len(stairway)-1):
+        _list.append((i, i+1, stairway[i]))
+        _list.append((i, i+2, stairway[i+1]))
+
+    _list.append((len(stairway)-1, len(stairway), stairway[-1]))
+    stairway_graph.add_weighted_edges_from(_list)
+
+
     print(stairway_path(stairway_graph))  # 72
--- Графы и деревья/Практические задания/Дейкстра. Кратчайший путь/task.py
+++ Графы и деревья/Практические задания/Дейкстра. Кратчайший путь/task.py
@@ -11,10 +11,30 @@ def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, U
     :param starting_node: Стартовый узел, откуда нужно начать обход
     :return: словарь как {'node1': 0, 'node2': 10, '3': 33, ...} со стоимостью путей, где node1, node2 - это узлы из графа g
     """
-    ...  # TODO вернуть стоимость путей до всех вершин посчитанных алгоритмом Дейкстры
+    # вернуть стоимость путей до всех вершин посчитанных алгоритмом Дейкстры
+    _, cost = nx.dijkstra_predecessor_and_distance(g, starting_node)
+
+    for node in g.nodes:
+        if node not in cost:
+            cost[node] = float('inf')
+    return cost
 
 
 if __name__ == '__main__':
-    ...  # TODO записать граф
+    # записать граф
 
-    print(dijkstra_algo(graph, 1))  # {1: 0, 2: 7, 3: 9, 6: 11, 4: 20, 5: 26}
+    g = nx.DiGraph()
+    g.add_weighted_edges_from([
+        (1, 2, 7),
+        (1, 3, 9),
+        (1, 6, 14),
+        (2, 3, 10),
+        (2, 4, 15),
+        (3, 4, 11),
+        (3, 6, 2),
+        (4, 5, 6),
+        (5, 6, 9)
+
+    ])
+
+    print(dijkstra_algo(g, 1))  # {1: 0, 2: 7, 3: 9, 6: 11, 4: 20, 5: 26}
